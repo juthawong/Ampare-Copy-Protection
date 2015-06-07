b8050248359cd640aab5cd0029e61e3babcd0e06
Juthawong Naisanguansee <juthawong@users.noreply.github.com>
2015-06-07 12:30:30 +0700

    Update amparecopyprotection.js

--- amparecopyprotection.js
+++ amparecopyprotection.js
@@ -1,15 +1,23 @@
 
-$(document).ready(function() {
-    $('html').on('mouseup mousedown', function(e) {
+jQuery(document).ready(function() {
+    jQuery('html').on('mouseup mousedown', function(e) {
     e.preventDefault();
 });
 
-    $("html").bind("contextmenu",function(){
+    jQuery("html").bind("contextmenu",function(){
        return false;
     }); 
 }); 
 
-
+jQuery(el).attr('unselectable','on')
+     .css({'-moz-user-select':'-moz-none',
+           '-moz-user-select':'none',
+           '-o-user-select':'none',
+           '-khtml-user-select':'none', /* you could also put this in a class */
+           '-webkit-user-select':'none',/* and add the CSS class here instead */
+           '-ms-user-select':'none',
+           'user-select':'none'
+     }).bind('selectstart', function(){ return false; });
 window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==70||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==70||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
 
 document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
